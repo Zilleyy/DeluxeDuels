ee0af6b7b6237bd4260763bb4de313b75600d4ad
BuildTools <unconfigured@null.spigotmc.org>
2021-03-08 16:47:32 -0600

    First version of LuxDuels

---
+++ src/main/java/com/prophaze/luxduels/LuxDuels.java
@@ -0,0 +1,12 @@
+package com.prophaze.luxduels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LuxDuels extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
