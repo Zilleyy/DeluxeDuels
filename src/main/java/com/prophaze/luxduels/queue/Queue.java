672de98217529ebfa6081aa3154763bbfd9ac053
Zilleyy <coopercowley2004@gmail.com>
2021-03-10 10:06:06 +1100

    HashMap

--- src/main/java/com/prophaze/luxduels/queue/Queue.java
+++ src/main/java/com/prophaze/luxduels/queue/Queue.java
@@ -1,7 +1,17 @@
 package com.prophaze.luxduels.queue;
 
+import com.prophaze.luxduels.match.MatchType;
 import com.prophaze.luxduels.profile.Profile;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Queue {
+
+    private HashMap<MatchType, LinkedList<Profile>> queue = new HashMap<>();
+
+    public static void addProfile(MatchType matchType, Profile profile) {
+        
+    }
 
 }
