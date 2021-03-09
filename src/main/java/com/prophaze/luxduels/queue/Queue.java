8907e60032c236c5fd8159000ef1c6317ade1491
Zilleyy <coopercowley2004@gmail.com>
2021-03-10 10:11:24 +1100

    Queue stuff

--- src/main/java/com/prophaze/luxduels/queue/Queue.java
+++ src/main/java/com/prophaze/luxduels/queue/Queue.java
@@ -10,8 +10,25 @@ public class Queue {
 
     private HashMap<MatchType, LinkedList<Profile>> queue = new HashMap<>();
 
-    public static void addProfile(MatchType matchType, Profile profile) {
-        
+    public void addProfile(MatchType matchType, Profile profile) {
+        if(this.queue.keySet().contains(matchType)) {
+            this.queue.get(matchType).add(profile);
+        } else {
+            LinkedList list = new LinkedList<Profile>();
+            list.add(profile);
+            this.queue.put(matchType, list);
+        }
+    }
+
+    public void removeProfile(Profile profile) {
+        for(MatchType matchType : this.queue.keySet()) {
+            for(Profile queued : this.queue.get(matchType)) {
+                if(queued.equals(profile)) {
+                    this.queue.get(matchType).remove(queued);
+                    return;
+                }
+            }
+        }
     }
 
 }
